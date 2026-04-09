a9936d621cdcadf0a3e1291a0009a365ba69167c
shakability <armaangulzar@gmail.com>
2026-04-09 13:42:59 -0700

    first update!!! final page is 22

--- pages.js
+++ pages.js
@@ -14,8 +14,349 @@ const pages = {
     3: {
         img: ["images/3.gif"],
         text: "",
-        cmd: "Ask why you're Roblox.",
+        cmd: "Examine room.",
         date: "2026-04-08"
+    }, 
+    4: {
+        img: ["images/4.png"],
+        text: "As was mentioned before, your name is QOOB. \nYou like doing THINGS, and then doing OTHER THINGS. \nEspecially THAT THING. \n\n\nYou get ecstatic just thinking about it. ",
+        cmd: "Ask why you're Roblox.",
+        date: "2026-04-09"
+    },
+    5: {
+        img: ["images/5.gif"],
+        text: "Okay man. Way to kick me in my shins when I'm already at my lowest. You KNOW I suck at drawing. \n\nWho knows. Maybe someday, if I get better at drawing, there'll coincidentally be a MAGICAL button that qoob presses that will instantly change the artstyle of the comic.",
+        cmd: "==>",
+        date: "2026-04-09"
+    },
+    6: {
+        img: ["images/6.gif"],
+        text: "",
+        cmd: "Pick up the phone.",
+        date: "2026-04-09"
+    },
+    7: {
+        img: ["images/7.png"],
+        text: "",
+        cmd: "Tell the readers.",
+        date: "2026-04-09",
+        chat: [
+            {
+                speaker: "queueBrute",
+                color: "#04afec",
+                text: "hello ??"
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "hey You"
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "yo are you There"
+            },
+            {
+                speaker: "queueBrute",
+                color: "#04afec",
+                text: "Wgat"
+            },
+            {
+                speaker: "queueBrute",
+                color: "#04afec",
+                text: "r u talking to me"
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "YES???"
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "I CALLED YOU WHO ELSE WOULD I BE TALKING TO"
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "DUMBASS"
+            },
+            {
+                speaker: "queueBrute",
+                color: "#04afec",
+                text: "Ok"
+            },
+            {
+                speaker: "queueBrute",
+                color: "#04afec",
+                text: "Why did you call me"
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "first to Tell you to Umm"
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "stop Doing that dance"
+            },
+            {
+                speaker: "queueBrute",
+                color: "#04afec",
+                text: "?"
+            },
+            {
+                speaker: "queueBrute",
+                color: "#04afec",
+                text: "Wat dance"
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "the dance You were doing"
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "kind of like 67"
+            },
+            {
+                speaker: "queueBrute",
+                color: "#04afec",
+                text: "Haha! Six Seven! "
+            },
+            {
+                speaker: "queueBrute",
+                color: "#04afec",
+                text: "I got that niche reference!"
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "Shut up"
+            },
+            {
+                speaker: "queueBrute",
+                color: "#04afec",
+                text: "Ok"
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "did You stop"
+            },
+            {
+                speaker: "queueBrute",
+                color: "#04afec",
+                text: "i stopped a while ago bro"
+            },
+            {
+                speaker: "queueBrute",
+                color: "#04afec",
+                text: "to... pick up the phone?"
+            },
+            {
+                speaker: "queueBrute",
+                color: "#04afec",
+                text: "why would i be doing the dance while on call"
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "oh"
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "yeah That makes sense"
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "i Can't see you right now because i'm on my phone right now calling you so"
+            },
+            {
+                speaker: "queueBrute",
+                color: "#04afec",
+                text: "Ok"
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "ok Um next i wanted To tell you"
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "to tell the READERS that they Can message me with requests on what they want You to do ."
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "Because I'm Unoriginal ."
+            },
+            {
+                speaker: "queueBrute",
+                color: "#04afec",
+                text: "Ok"
+            },
+            {
+                speaker: "queueBrute",
+                color: "#04afec",
+                text: "Is tgat all"
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "yeah i Think so"
+            },
+            {
+                speaker: "queueBrute",
+                color: "#04afec",
+                text: "we have A lot of unnecessary dialogue"
+            },
+            {
+                speaker: "queueBrute",
+                color: "#04afec",
+                text: "after all i'm fictional. You're just sitting and typing this dialogue between you and another person who is spiritually you"
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "you Need to know when to stop talking bro"
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "patience is a virtue"
+            },
+            {
+                speaker: "queueBrute",
+                color: "#04afec",
+                text: "Ok"
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "i Think i'm gonna go now"
+            },
+            {
+                speaker: "queueBrute",
+                color: "#04afec",
+                text: "Oh"
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "yeah"
+            },
+            {
+                speaker: "queueBrute",
+                color: "#04afec",
+                text: "Cough"
+            },
+            {
+                speaker: "shakability",
+                color: "#ff5d5e",
+                text: "ok Bye"
+            },
+            {
+                speaker: "queueBrute",
+                color: "#04afec",
+                text: "Bye"
+            }
+        ]
+    },
+    8: {
+        img: ["images/8.png"],
+        text: "So that's that! YOU guys tell ME what to do. This is gonna be great!\n\nI'm just now realizing that this is stupid. You guys saw me tell him to tell you guys. So you knew before he even told you. And technically by pressing the button you guys were the ones who made him tell you. \n\nidk let's just move on",
+        cmd: "@ghostkyle95 asks",
+        date: "2026-04-09"
+    },
+    9: {
+        img: ["images/9_1.png","images/9_2.png"],
+        text: "",
+        cmd: "==>",
+        date: "2026-04-09"
+    },
+    10: {
+        img: ["images/9_3.png"],
+        text: "",
+        cmd: "==>",
+        date: "2026-04-09"
+    },
+    11: {
+        img: ["images/9_2.png"],
+        text: "",
+        cmd: "==>",
+        date: "2026-04-09"
+    },
+    12: {
+        img: ["images/9_3.png"],
+        text: "",
+        cmd: "==>",
+        date: "2026-04-09"
+    },
+    13: {
+        img: ["images/9_2.png"],
+        text: "",
+        cmd: "==>",
+        date: "2026-04-09"
+    },
+    14: {
+        img: ["images/9_4.png"],
+        text: "",
+        cmd: "==>",
+        date: "2026-04-09"
+    },
+    15: {
+        img: ["images/8.png"],
+        text: "Anyways!!! Moving on now!!!",
+        cmd: "@frosber asks",
+        date: "2026-04-09"
+    },
+    16: {
+        img: ["images/10_1.png", "images/9_2.png"],
+        text: "",
+        cmd: "==>",
+        date: "2026-04-09"
+    },
+    17: {
+        img: ["images/10_3.png"],
+        text: "",
+        cmd: "==>",
+        date: "2026-04-09"
+    },
+    18: {
+        img: ["images/10_4.png"],
+        text: "",
+        cmd: "==>",
+        date: "2026-04-09"
+    },
+    19: {
+        img: ["images/8.png"],
+        text: "Come on!!! You guys are so lame! Give me something with more PIZZAZZ.",
+        cmd: "@shakability asks",
+        date: "2026-04-09"
+    },
+    20: {
+        img: ["images/20.png"],
+        text: "Now THAT's more like it!",
+        cmd: "Hit it!",
+        date: "2026-04-09"
+    },
+    21: {
+        img: ["images/21.html"],
+        text: "",
+        cmd: "==>",
+        date: "2026-04-09"
+    },
+    22: {
+        img: ["images/8.png"],
+        text: "What's next? KEEP MESSAGING ME!!!",
+        cmd: "==>",
+        date: "2026-04-09"
     }
 };
 
